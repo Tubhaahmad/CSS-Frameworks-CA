ddef8ecc7cb22fe48b7349326687c80546dd68f0
Tubha Ahmad <toubha.ah@hotmail.com>
2025-11-03 14:23:45 +0100

    Installed Sass and Bootstrap correctly

--- CSS-frameworks-CA/src/main.js
+++ CSS-frameworks-CA/src/main.js
@@ -2,6 +2,9 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/scss/bootstrap";
+
 
 document.querySelector('#app').innerHTML = `
   <div>
